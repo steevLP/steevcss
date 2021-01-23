5563157e30d42364e4f0bd5d127a1d8ed9bc83fd
steevLP <mauricefl@outlook.de>
2021-01-24 00:48:59 +0100

    added some weird fixes to prevent navbar overlapping

--- dist/ef19487056b129c8f3eda17f9543ab70.js
+++ dist/ef19487056b129c8f3eda17f9543ab70.js
@@ -149,7 +149,7 @@ function Module() {
 module.bundle.Module = Module;
 
 if (!module.bundle.parent && typeof WebSocket !== 'undefined') {
-  var ws = new WebSocket('ws://localhost:56486/');
+  var ws = new WebSocket('ws://localhost:58854/');
   ws.onmessage = function(event) {
     var data = JSON.parse(event.data);
 
